89585af955733e313347cf7b6bf8234e53fa2c33
kalbek <44664009+kalbek@users.noreply.github.com>
2022-04-27 14:26:45 +0300

    Auth, page Setup, and schoo CRuDs completed

---
+++ frontend/src/features/schools/schoolSlice.js
@@ -0,0 +1,103 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import schoolService from './schoolService'
+const initialState = {
+    schools: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+//Create new School
+export const createSchool = createAsyncThunk('schools/create', async (schoolData, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        // remove the token for no auth school creation
+        return await schoolService.createSchool(schoolData, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message)
+        || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+// Get Schools
+export const getSchools = createAsyncThunk('schools/getAll', async (_, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        // remove the token for no auth school creation
+        return await schoolService.getSchools(token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message)
+        || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+//Delete user School
+export const deleteSchool = createAsyncThunk('schools/delete', async (id, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        // remove the token for no auth school creation
+        return await schoolService.deleteSchool(id, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message)
+        || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+export const schoolSlice = createSlice({
+    name: 'school',
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    }, 
+    extraReducers: (builder) => {
+        builder
+        .addCase(createSchool.pending, (state) => {
+            state.isLoading = true
+        })
+        .addCase(createSchool.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.schools.push(action.payload)
+        })
+        .addCase(createSchool.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message(action.payload)
+        })
+
+        // to get schools
+        .addCase(getSchools.pending, (state) => {
+            state.isLoading = true
+        })
+        .addCase(getSchools.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.schools = action.payload
+        })
+        .addCase(getSchools.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message(action.payload)
+        })
+
+        // to delete schools
+        .addCase(deleteSchool.pending, (state) => {
+            state.isLoading = true
+        })
+        .addCase(deleteSchool.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.schools = state.schools.filter((school) => school._id !== action.payload.id)
+        })
+        .addCase(deleteSchool.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+        })
+    },
+})
+
+export const {reset} = schoolSlice.actions
+export default schoolSlice.reducer
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,53 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import SchoolForm from '../components/SchoolForm'
+import SchoolItem from '../components/SchoolItem'
+import Spinner from '../components/Spinner'
+import { getSchools,reset } from '../features/schools/schoolSlice'
 
 function Dashboard() {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const { user } = useSelector((state) => state.auth)
+  const {schools, isLoading, isError, message} = useSelector((state) => state.schools)
   useEffect(() => {
+    if (isError){
+      console.log(message)
+    }
     if (!user) {
       navigate('/login')
     }
-  }, [user, navigate])
-  return (
-    <div>Dashboard</div>
+
+    dispatch(getSchools())
+
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate, isError, message, dispatch])
+
+    if (isLoading){
+      return <Spinner/>
+    }
+    return (
+      <> 
+      <section>
+        <h4 className='user-name'>Welcome {user && user.name.charAt(0).toUpperCase()+ user.name.slice(1)}</h4>
+        <h1>Dashboard</h1>
+      </section>
+      <SchoolForm/>
+
+      <section className="content">
+        {schools.length > 0 ? (
+          <div className="schools">
+            {schools.map((school) => (
+                <SchoolItem key={school._id} school={school}/>
+            ))}
+          </div>
+        ) : (<h3>No schools found</h3>)}
+      </section>
+    </>
   )
 }
 
